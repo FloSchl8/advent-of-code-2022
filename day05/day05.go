a18f9fc4f09910d4130ca81e37814f0f6c182b02
Florian Schlund <schlundi8@gmail.com>
2022-12-05 20:49:40 +0100

    day 5 part 1

---
+++ day05/day05.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type operation struct {
+	numOfCrates int
+	pickup      int
+	dropAt      int
+}
+
+func main() {
+	input, err := os.ReadFile("./day05/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Top crates: ", stackCrates(string(input)))
+}
+
+func stackCrates(input string) (topOnes string) {
+	lines := strings.Split(input, "\n")
+
+	instructionsStartAfterLine := findSeperatorLine(lines)
+
+	crates := getNumberOfCrates(lines[instructionsStartAfterLine-1])
+
+	stacks := make([][]string, crates)
+
+	for i := 0; i < crates; i++ {
+		stacks[i] = make([]string, instructionsStartAfterLine-1)
+		for j := 0; j < instructionsStartAfterLine-1; j++ {
+			line := lines[j]
+			if crate := string([]rune(line)[i*4+1]); crate != " " {
+				stacks[i][j] = crate
+			}
+		}
+	}
+
+	fmt.Println(stacks)
+
+	operations := make([]operation, 0)
+
+	for i := instructionsStartAfterLine + 1; i < len(lines); i++ {
+		if lines[i] != "" {
+			operations = append(operations, getOperation(lines[i]))
+		}
+	}
+
+	for k, o := range operations {
+		for i := 0; i < o.numOfCrates; i++ {
+			pickupStack := stacks[o.pickup-1]
+			c := ""
+			var newStack []string
+			for j := 0; j < len(pickupStack); j++ {
+				if pickupStack[j] != "" {
+					c, newStack = pickupStack[j], pickupStack[j+1:]
+					break
+				}
+			}
+			stacks[o.pickup-1] = newStack
+
+			dropAtStack := stacks[o.dropAt-1]
+			if len(dropAtStack) == 0 {
+				dropAtStack = append(dropAtStack, c)
+			} else {
+				for j := 0; j < len(dropAtStack); j++ {
+					if dropAtStack[j] != "" {
+						firstPart := dropAtStack[:j]
+						secondPart := append([]string{c}, dropAtStack[j:]...)
+						dropAtStack = append(firstPart, secondPart...)
+						break
+					}
+				}
+			}
+			stacks[o.dropAt-1] = dropAtStack
+		}
+		fmt.Println("operation: ", k, "stacks: ", stacks)
+
+	}
+	result := ""
+	for _, stack := range stacks {
+		for i := 0; i < len(stack); i++ {
+			if stack[i] != "" {
+				result += stack[i]
+				break
+			}
+		}
+	}
+
+	return result
+}
+
+func findSeperatorLine(lines []string) (instructionsStartAfterLine int) {
+	for i, line := range lines {
+		if line == "" {
+			return i
+		}
+	}
+	return 0
+}
+
+func getNumberOfCrates(input string) int {
+	return len(strings.Split(input, "   "))
+}
+
+func getOperation(operationInput string) operation {
+	split := strings.Split(operationInput, " ")
+
+	numOfCrates, _ := strconv.Atoi(split[1])
+	pickup, _ := strconv.Atoi(split[3])
+	dropAt, _ := strconv.Atoi(split[5])
+
+	return operation{
+		numOfCrates: numOfCrates,
+		pickup:      pickup,
+		dropAt:      dropAt,
+	}
+}
---
+++ day05/day05_test.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_stackCrates(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantTopOnes string
+	}{
+		{name: "testinput", args: args{input: `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+`}, wantTopOnes: "CMZ"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotTopOnes := stackCrates(tt.args.input); gotTopOnes != tt.wantTopOnes {
+				t.Errorf("stackCrates() = %v, want %v", gotTopOnes, tt.wantTopOnes)
+			}
+		})
+	}
+}
+
+func Test_findSeperatorLine(t *testing.T) {
+	type args struct {
+		lines []string
+	}
+	tests := []struct {
+		name                           string
+		args                           args
+		wantInstructionsStartAfterLine int
+	}{
+		{name: "testinput", args: args{lines: []string{"23234", "fwef", "", "werwer"}}, wantInstructionsStartAfterLine: 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotInstructionsStartAfterLine := findSeperatorLine(tt.args.lines); gotInstructionsStartAfterLine != tt.wantInstructionsStartAfterLine {
+				t.Errorf("findSeperatorLine() = %v, want %v", gotInstructionsStartAfterLine, tt.wantInstructionsStartAfterLine)
+			}
+		})
+	}
+}
+
+func Test_getNumberOfStacks(t *testing.T) {
+	type args struct {
+		input string
+	}
+	test := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "testinput",
+			args: args{input: " 1   2   3 "},
+			want: 3,
+		},
+	}
+	for _, tt := range test {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getNumberOfCrates(tt.args.input); got != tt.want {
+				t.Errorf("findSeperatorLine() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getOperation(t *testing.T) {
+	type args struct {
+		operationInput string
+	}
+	tests := []struct {
+		name string
+		args args
+		want operation
+	}{
+		{name: "move 1 from 2 to 1", args: args{"move 1 from 2 to 1"}, want: operation{
+			numOfCrates: 1,
+			pickup:      2,
+			dropAt:      1,
+		}},
+		{name: "move 3 from 1 to 3", args: args{"move 3 from 1 to 3"}, want: operation{
+			numOfCrates: 3,
+			pickup:      1,
+			dropAt:      3,
+		}},
+		{name: "move 2 from 2 to 1", args: args{"move 2 from 2 to 1"}, want: operation{
+			numOfCrates: 2,
+			pickup:      2,
+			dropAt:      1,
+		}},
+		{name: "move 1 from 1 to 2", args: args{"move 1 from 1 to 2"}, want: operation{
+			numOfCrates: 1,
+			pickup:      1,
+			dropAt:      2,
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getOperation(tt.args.operationInput); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getOperation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
