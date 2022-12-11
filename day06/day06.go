fe6dfe380fda1958f55bf41538356c6296366b86
Florian Schlund <schlundi8@gmail.com>
2022-12-11 11:47:32 +0100

    day 6 part 2

--- day06/day06.go
+++ day06/day06.go
@@ -12,6 +12,8 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("Part 1", "Signal after:", part01(string(input)))
+	fmt.Println("Part 2", "Message after:", part02(string(input)))
+
 }
 
 func part01(input string) int {
@@ -24,6 +26,20 @@ func part01(input string) int {
 		}
 		if len(set) == 4 {
 			return i + 4
+		}
+	}
+	return 0
+}
+
+func part02(input string) int {
+	for i := 0; i < len(input); i++ {
+		set := make(map[string]interface{})
+		seq := input[i : i+14]
+		for _, s := range seq {
+			set[string(s)] = ""
+		}
+		if len(set) == 14 {
+			return i + 14
 		}
 	}
 	return 0
--- day06/day06_test.go
+++ day06/day06_test.go
@@ -25,3 +25,26 @@ func Test_part01(t *testing.T) {
 		})
 	}
 }
+
+func Test_part2(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "testinput 1", args: args{input: "mjqjpqmgbljsphdztnvjfqwrcgsmlb"}, want: 19},
+		{name: "testinput 2", args: args{input: "bvwbjplbgvbhsrlpgdmjqwftvncz"}, want: 23},
+		{name: "testinput 3", args: args{input: "nppdvjthqldpwncqszvftbrmjlhg"}, want: 23},
+		{name: "testinput 4", args: args{input: "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"}, want: 29},
+		{name: "testinput 5", args: args{input: "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"}, want: 26}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part02(tt.args.input); got != tt.want {
+				t.Errorf("part02() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
