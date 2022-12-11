fc0f044fad0ce047e2dc3f981a2fab5219db25ed
Florian Schlund <schlundi8@gmail.com>
2022-12-11 21:44:00 +0100

    day 7 part 1

---
+++ day07/day07.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type directory struct {
+	name           string
+	files          map[string]*file
+	subdirectories map[string]*directory
+	parent         *directory
+	size           int
+}
+
+type file struct {
+	name string
+	size int
+}
+
+func (d *directory) addFile(newFile *file) {
+	if d.files != nil {
+		d.files[newFile.name] = newFile
+	} else {
+		d.files = make(map[string]*file)
+		d.files[newFile.name] = newFile
+	}
+}
+
+func (d *directory) getDirectorySize() int {
+	sum := 0
+	for _, f := range d.files {
+		sum += f.size
+	}
+	for _, subdirectory := range d.subdirectories {
+		sum += subdirectory.getDirectorySize()
+	}
+	d.size = sum
+	return sum
+}
+
+func (d *directory) addSubDirectory(subDirectory *directory) {
+	if d.subdirectories != nil {
+		d.subdirectories[subDirectory.name] = subDirectory
+	} else {
+		d.subdirectories = make(map[string]*directory)
+		d.subdirectories[subDirectory.name] = subDirectory
+	}
+}
+
+func main() {
+	input, err := os.ReadFile("./day07/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Part 01", "Total size of directories not bigger than 100.000", part01(string(input)))
+}
+
+func part01(input string) int {
+	lines := strings.Split(input, "\n")
+
+	root := &directory{name: "/"}
+
+	currentDirectory := root
+
+	for _, line := range lines {
+		if isCommand(line) {
+			commandSplit := strings.Split(line, " ")
+			if commandSplit[1] == "cd" {
+				if len(commandSplit) == 3 && commandSplit[2] != "/" && commandSplit[2] != ".." { // '$ cd foo'
+					for _, subdirectory := range currentDirectory.subdirectories {
+						if subdirectory.name == commandSplit[2] {
+							currentDirectory = subdirectory
+						}
+					}
+				} else if len(commandSplit) == 3 {
+					if commandSplit[2] == "/" { // '$ cd /'
+						currentDirectory = root
+					} else if commandSplit[2] == ".." { // '$ cd ..'
+						currentDirectory = currentDirectory.parent
+					}
+				}
+			}
+		}
+		if isDirectory(line) {
+			sub := &directory{name: strings.Split(line, " ")[1], parent: currentDirectory}
+			currentDirectory.addSubDirectory(sub)
+		}
+
+		if isFile(line) {
+			f := strings.Split(line, " ")
+			file := &file{
+				name: f[1],
+			}
+			if size, _ := strconv.Atoi(f[0]); size != 0 {
+				file.size = size
+			}
+			currentDirectory.addFile(file)
+		}
+	}
+
+	root.getDirectorySize()
+
+	var dirSizes = make(map[string]int)
+	for s, d := range root.subdirectories {
+		dirSizes["/"+s] = d.size
+		getSubdirecotrySize(d, dirSizes, s)
+	}
+
+	result := 0
+
+	for s, i := range dirSizes {
+		if i <= 100000 {
+			result += dirSizes[s]
+		}
+		fmt.Println(s, ":", dirSizes[s])
+	}
+
+	return result
+}
+
+func getSubdirecotrySize(d *directory, dirSizes map[string]int, parent string) {
+	for s, d := range d.subdirectories {
+		dirSizes[parent+"/"+s] = d.size
+		getSubdirecotrySize(d, dirSizes, s)
+	}
+}
+
+func isFile(input string) bool {
+	regex := regexp.MustCompile(`\d+`)
+	return len(regex.FindStringIndex(input)) > 0
+}
+
+func isDirectory(input string) bool {
+	return strings.HasPrefix(input, "dir")
+}
+
+func isCommand(input string) bool {
+	return strings.HasPrefix(input, "$")
+}
---
+++ day07/day07_test.go
@@ -0,0 +1,168 @@
+package main
+
+import "testing"
+
+func Test_part01(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "testinput", args: args{input: `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+`}, want: 95437},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part01(tt.args.input); got != tt.want {
+				t.Errorf("part01() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isCommand(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "$ ls", args: args{input: "$ ls"}, want: true},
+		{name: "dir a", args: args{input: "dir a"}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isCommand(tt.args.input); got != tt.want {
+				t.Errorf("isCommand() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_directory_getDirectorySize(t *testing.T) {
+	type fields struct {
+		name           string
+		files          map[string]*file
+		subdirectories map[string]*directory
+		parent         *directory
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int
+	}{
+		{
+			name: "test 1 file", fields: fields{
+				name: "a",
+				files: map[string]*file{"a": {
+					name: "a",
+					size: 10,
+				}},
+				subdirectories: nil,
+				parent:         nil,
+			}, want: 10,
+		},
+		{
+			name: "test 2 files", fields: fields{
+				name: "a",
+				files: map[string]*file{"a": {
+					name: "a",
+					size: 10,
+				}, "b": {
+					name: "b",
+					size: 10,
+				}},
+				subdirectories: nil,
+				parent:         nil,
+			}, want: 20,
+		},
+		{
+			name: "test 1 subdirectory", fields: fields{
+				name:  "root",
+				files: nil,
+				subdirectories: map[string]*directory{
+					"a": {files: map[string]*file{"a": {
+						name: "a",
+						size: 10,
+					}}},
+				},
+				parent: nil,
+			},
+			want: 10,
+		},
+		{
+			name: "test 2 subdirectory", fields: fields{
+				name:  "root",
+				files: nil,
+				subdirectories: map[string]*directory{
+					"a": {files: map[string]*file{"a": {
+						name: "a",
+						size: 10,
+					}}},
+					"b": {files: map[string]*file{"b": {
+						name: "b",
+						size: 10,
+					}}},
+				},
+				parent: nil,
+			},
+			want: 20,
+		}, {
+			name: "1 file 1 subdirectory", fields: fields{
+				name: "root",
+				files: map[string]*file{"a": {
+					name: "a",
+					size: 10,
+				}},
+				subdirectories: map[string]*directory{
+					"a": {files: map[string]*file{"a": {
+						name: "a",
+						size: 10,
+					}}},
+				},
+				parent: nil,
+			},
+			want: 20,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &directory{
+				name:           tt.fields.name,
+				files:          tt.fields.files,
+				subdirectories: tt.fields.subdirectories,
+				parent:         tt.fields.parent,
+			}
+			if got := d.getDirectorySize(); got != tt.want {
+				t.Errorf("getDirectorySize() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
