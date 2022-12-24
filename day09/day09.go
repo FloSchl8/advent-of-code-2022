42050c43f0d4ea3146e87479298ee308c51f98cb
Florian Schlund <schlundi8@gmail.com>
2022-12-24 11:06:07 +0100

    day 9 part 1

---
+++ day09/day09.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type move struct {
+	steps     int
+	direction string
+}
+
+type point struct {
+	x, y int
+}
+
+func (t *tail) distanceFromHead(head head) float64 {
+	return math.Sqrt(math.Pow(float64(head.x-t.x), 2) + math.Pow(float64(head.y-t.y), 2))
+}
+
+func (p *point) pointIsEqual(other point) bool {
+	return p.x == other.x && p.y == other.y
+}
+
+type head point
+type tail point
+
+func main() {
+	input, err := os.ReadFile("./day09/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Part 1", "Number of unique visited points of tail", part01(string(input)))
+}
+
+func part01(input string) int {
+	lines := strings.Split(input, "\n")
+
+	var moves = []move{}
+
+	for _, line := range lines {
+		if line != "" {
+			moves = append(moves, getMoveFromLine(line))
+		}
+	}
+
+	h := head{x: 0, y: 0}
+	t := tail{x: 0, y: 0}
+
+	var tailPositions = []point{}
+
+	for _, m := range moves {
+		for i := 0; i < m.steps; i++ {
+			moveHead(&h, m.direction)
+
+			fromHead := t.distanceFromHead(h)
+			if fromHead > math.Sqrt2 {
+				moveTail(&t, &h)
+			}
+			tailPositions = append(tailPositions, point(t))
+		}
+	}
+
+	var uniquePositions = []point{}
+
+	for i, position := range tailPositions {
+		unique := true
+		for j := 0; j < len(uniquePositions); j++ {
+			if i != j {
+				if position.pointIsEqual(uniquePositions[j]) {
+					unique = false
+				}
+			}
+		}
+		if unique {
+			uniquePositions = append(uniquePositions, position)
+		}
+	}
+
+	return len(uniquePositions)
+}
+
+func moveTail(t *tail, h *head) {
+	// horizontal
+	if t.y == t.y && t.x != h.x {
+		if t.x-h.x > 0 { // tail right of head
+			t.x--
+		} else {
+			t.x++
+		}
+	}
+	// vertical
+	if t.x == t.x && t.y != h.y {
+		if t.y-h.y > 0 { // tail above of head
+			t.y--
+		} else {
+			t.y++
+		}
+	}
+}
+
+func moveHead(h *head, direction string) {
+	switch direction {
+	case "R":
+		h.y++
+	case "U":
+		h.x++
+	case "L":
+		h.y--
+	case "D":
+		h.x--
+	}
+
+}
+
+func getMoveFromLine(line string) move {
+	split := strings.Split(line, " ")
+	steps, _ := strconv.Atoi(split[1])
+	return move{
+		steps:     steps,
+		direction: split[0],
+	}
+}
---
+++ day09/day09_test.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"math"
+	"reflect"
+	"testing"
+)
+
+func Test_tail_distanceFromHead(t *testing.T) {
+	type args struct {
+		head head
+	}
+	tests := []struct {
+		name string
+		t    tail
+		args args
+		want float64
+	}{
+		{
+			name: "head right of tail is 1", args: args{head: head{
+				x: 1,
+				y: 0,
+			}}, t: tail{
+				x: 0,
+				y: 0,
+			}, want: 1,
+		},
+		{
+			name: "head 1 right 1 up of tail is sqrt(2)", args: args{head: head{
+				x: 1,
+				y: 1,
+			}}, t: tail{
+				x: 0,
+				y: 0,
+			}, want: math.Sqrt2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.t.distanceFromHead(tt.args.head); got != tt.want {
+				t.Errorf("distanceFromHead() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_part01(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "testinput",
+			args: args{input: `R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
+`}, want: 13,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part01(tt.args.input); got != tt.want {
+				t.Errorf("part01() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getMoveFromLine(t *testing.T) {
+	type args struct {
+		line string
+	}
+	tests := []struct {
+		name string
+		args args
+		want move
+	}{
+		{
+			name: "R 4 gets right with 4 steps",
+			args: args{line: "R 4"},
+			want: move{
+				steps:     4,
+				direction: "R",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getMoveFromLine(tt.args.line); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getMoveFromLine() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_moveTail(t *testing.T) {
+	type args struct {
+		t *tail
+		h *head
+	}
+	tests := []struct {
+		name string
+		args args
+		want *tail
+	}{
+		{
+			name: "head moved right", args: args{
+				t: &tail{
+					x: 0,
+					y: 0,
+				},
+				h: &head{
+					x: 2,
+					y: 0,
+				},
+			}, want: &tail{
+				x: 1,
+				y: 0,
+			},
+		},
+		{
+			name: "head moved left", args: args{
+				t: &tail{
+					x: 2,
+					y: 0,
+				},
+				h: &head{
+					x: 0,
+					y: 0,
+				},
+			}, want: &tail{
+				x: 1,
+				y: 0,
+			},
+		},
+		{
+			name: "head moved up", args: args{
+				t: &tail{
+					x: 0,
+					y: 0,
+				},
+				h: &head{
+					x: 0,
+					y: 2,
+				},
+			}, want: &tail{
+				x: 0,
+				y: 1,
+			},
+		},
+		{
+			name: "head moved down", args: args{
+				t: &tail{
+					x: 0,
+					y: 2,
+				},
+				h: &head{
+					x: 0,
+					y: 0,
+				},
+			}, want: &tail{
+				x: 0,
+				y: 1,
+			},
+		},
+		{
+			name: "head moved up and right", args: args{
+				t: &tail{
+					x: 0,
+					y: 0,
+				},
+				h: &head{
+					x: 2,
+					y: 2,
+				},
+			}, want: &tail{
+				x: 1,
+				y: 1,
+			},
+		},
+		{
+			name: "head moved up and left", args: args{
+				t: &tail{
+					x: 0,
+					y: 0,
+				},
+				h: &head{
+					x: 2,
+					y: -2,
+				},
+			}, want: &tail{
+				x: 1,
+				y: -1,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			moveTail(tt.args.t, tt.args.h)
+			if !reflect.DeepEqual(tt.args.t, tt.want) {
+				t.Errorf("moveTail() = %v, want %v", tt.args.t, tt.want)
+			}
+		})
+	}
+}
