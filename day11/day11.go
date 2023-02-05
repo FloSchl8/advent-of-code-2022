11c61db0c2bab373cab3a61b320809de75b46f3d
Florian Schlund <schlundi8@gmail.com>
2023-02-05 19:52:33 +0100

    day 11 part 2

--- day11/day11.go
+++ day11/day11.go
@@ -8,7 +8,7 @@ import (
 type monkey struct {
 	items      []int
 	operation  func(old int) int
-	test       func(worrylevel int) bool
+	testMod    int
 	trueThrow  int
 	falseThrow int
 }
@@ -20,9 +20,7 @@ func main() {
 		operation: func(old int) int {
 			return old * 2
 		},
-		test: func(worrylevel int) bool {
-			return worrylevel%17 == 0
-		},
+		testMod:    17,
 		trueThrow:  2,
 		falseThrow: 5,
 	}, {
@@ -30,9 +28,7 @@ func main() {
 		operation: func(old int) int {
 			return old + 3
 		},
-		test: func(worrylevel int) bool {
-			return worrylevel%13 == 0
-		},
+		testMod:    13,
 		trueThrow:  7,
 		falseThrow: 4,
 	}, {
@@ -40,9 +36,7 @@ func main() {
 		operation: func(old int) int {
 			return old + 6
 		},
-		test: func(worrylevel int) bool {
-			return worrylevel%19 == 0
-		},
+		testMod:    19,
 		trueThrow:  6,
 		falseThrow: 5,
 	}, {
@@ -50,9 +44,7 @@ func main() {
 		operation: func(old int) int {
 			return old + 5
 		},
-		test: func(worrylevel int) bool {
-			return worrylevel%7 == 0
-		},
+		testMod:    7,
 		trueThrow:  7,
 		falseThrow: 1,
 	}, {
@@ -60,9 +52,7 @@ func main() {
 		operation: func(old int) int {
 			return old + 8
 		},
-		test: func(worrylevel int) bool {
-			return worrylevel%11 == 0
-		},
+		testMod:    11,
 		trueThrow:  0,
 		falseThrow: 2,
 	}, {
@@ -70,9 +60,7 @@ func main() {
 		operation: func(old int) int {
 			return old * 5
 		},
-		test: func(worrylevel int) bool {
-			return worrylevel%3 == 0
-		},
+		testMod:    3,
 		trueThrow:  6,
 		falseThrow: 3,
 	}, {
@@ -80,9 +68,7 @@ func main() {
 		operation: func(old int) int {
 			return old * old
 		},
-		test: func(worrylevel int) bool {
-			return worrylevel%2 == 0
-		},
+		testMod:    2,
 		trueThrow:  3,
 		falseThrow: 1,
 	}, {
@@ -90,39 +76,87 @@ func main() {
 		operation: func(old int) int {
 			return old + 4
 		},
-		test: func(worrylevel int) bool {
-			return worrylevel%5 == 0
-		},
+		testMod:    5,
 		trueThrow:  4,
 		falseThrow: 0,
 	},
 	}
 
 	fmt.Println("Part 1", "Monkey business", part01(monkeys))
+	// since we're using pointers our initial values need to be restored
+	monkeys = []*monkey{{
+		items: []int{96, 60, 68, 91, 83, 57, 85},
+		operation: func(old int) int {
+			return old * 2
+		},
+		testMod:    17,
+		trueThrow:  2,
+		falseThrow: 5,
+	}, {
+		items: []int{75, 78, 68, 81, 73, 99},
+		operation: func(old int) int {
+			return old + 3
+		},
+		testMod:    13,
+		trueThrow:  7,
+		falseThrow: 4,
+	}, {
+		items: []int{69, 86, 67, 55, 96, 69, 94, 85},
+		operation: func(old int) int {
+			return old + 6
+		},
+		testMod:    19,
+		trueThrow:  6,
+		falseThrow: 5,
+	}, {
+		items: []int{88, 75, 74, 98, 80},
+		operation: func(old int) int {
+			return old + 5
+		},
+		testMod:    7,
+		trueThrow:  7,
+		falseThrow: 1,
+	}, {
+		items: []int{82},
+		operation: func(old int) int {
+			return old + 8
+		},
+		testMod:    11,
+		trueThrow:  0,
+		falseThrow: 2,
+	}, {
+		items: []int{72, 92, 92},
+		operation: func(old int) int {
+			return old * 5
+		},
+		testMod:    3,
+		trueThrow:  6,
+		falseThrow: 3,
+	}, {
+		items: []int{74, 61},
+		operation: func(old int) int {
+			return old * old
+		},
+		testMod:    2,
+		trueThrow:  3,
+		falseThrow: 1,
+	}, {
+		items: []int{76, 86, 83, 55},
+		operation: func(old int) int {
+			return old + 4
+		},
+		testMod:    5,
+		trueThrow:  4,
+		falseThrow: 0,
+	},
+	}
+	fmt.Println("Part 2", "Monkey business", part02(monkeys))
+
 }
 
 func part01(monkeys []*monkey) int {
 
-	monkeyCounter := map[int]int{}
-
-	for i := 0; i < len(monkeys); i++ {
-		monkeyCounter[i] = 0
-	}
-
-	for i := 0; i < 20; i++ {
-		for i, m := range monkeys {
-			for _, item := range m.items {
-				newLevel := m.operation(item) / 3
-				monkeyCounter[i]++
-				if m.test(newLevel) {
-					monkeys[m.trueThrow].items = append(monkeys[m.trueThrow].items, newLevel)
-				} else {
-					monkeys[m.falseThrow].items = append(monkeys[m.falseThrow].items, newLevel)
-				}
-			}
-			m.items = nil
-		}
-	}
+	monkeyCounter := playMonkeyBuisness(monkeys, 20, 0)
 
 	result := 1
 	ints := []int{}
@@ -136,4 +170,60 @@ func part01(monkeys []*monkey) int {
 	}
 
 	return result
+}
+
+func part02(monkeys []*monkey) int {
+
+	commonDiv := 1
+
+	for _, m := range monkeys {
+		commonDiv *= m.testMod
+	}
+
+	monkeyCounter := playMonkeyBuisness(monkeys, 10000, commonDiv)
+
+	result := 1
+	ints := []int{}
+	for _, i := range monkeyCounter {
+		ints = append(ints, i)
+	}
+	sort.Ints(ints)
+
+	for _, i := range ints[len(ints)-2:] {
+		result *= i
+	}
+
+	return result
+}
+
+func playMonkeyBuisness(monkeys []*monkey, rounds, commonDiv int) map[int]int {
+	monkeyCounter := map[int]int{}
+
+	for i := 0; i < len(monkeys); i++ {
+		monkeyCounter[i] = 0
+	}
+
+	for j := 0; j < rounds; j++ {
+		for i, m := range monkeys {
+			for _, item := range m.items {
+				newLevel := 0
+				if commonDiv == 0 {
+					newLevel = m.operation(item) / 3
+				} else {
+					newLevel = m.operation(item) % commonDiv
+				}
+				monkeyCounter[i]++
+				if newLevel%m.testMod == 0 {
+					monkeys[m.trueThrow].items = append(monkeys[m.trueThrow].items, newLevel)
+				} else {
+					monkeys[m.falseThrow].items = append(monkeys[m.falseThrow].items, newLevel)
+				}
+			}
+			m.items = nil
+		}
+		if j%1000 == 0 {
+			fmt.Println("Round", j, monkeyCounter)
+		}
+	}
+	return monkeyCounter
 }
--- day11/day11_test.go
+++ day11/day11_test.go
@@ -11,9 +11,7 @@ func Test_part01(t *testing.T) {
 		operation: func(old int) int {
 			return old * 19
 		},
-		test: func(worrylevel int) bool {
-			return worrylevel%23 == 0
-		},
+		testMod:    23,
 		trueThrow:  2,
 		falseThrow: 3,
 	}, {
@@ -21,9 +19,7 @@ func Test_part01(t *testing.T) {
 		operation: func(old int) int {
 			return old + 6
 		},
-		test: func(worrylevel int) bool {
-			return worrylevel%19 == 0
-		},
+		testMod:    19,
 		trueThrow:  2,
 		falseThrow: 0,
 	}, {
@@ -31,9 +27,7 @@ func Test_part01(t *testing.T) {
 		operation: func(old int) int {
 			return old * old
 		},
-		test: func(worrylevel int) bool {
-			return worrylevel%13 == 0
-		},
+		testMod:    13,
 		trueThrow:  1,
 		falseThrow: 3,
 	}, {
@@ -41,9 +35,7 @@ func Test_part01(t *testing.T) {
 		operation: func(old int) int {
 			return old + 3
 		},
-		test: func(worrylevel int) bool {
-			return worrylevel%17 == 0
-		},
+		testMod:    17,
 		trueThrow:  0,
 		falseThrow: 1,
 	},
@@ -63,6 +55,64 @@ func Test_part01(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := part01(tt.args.monkeys); got != tt.want {
 				t.Errorf("part01() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_part02(t *testing.T) {
+	type args struct {
+		monkeys []*monkey
+	}
+	testMonkeys := []*monkey{{
+		items: []int{79, 98},
+		operation: func(old int) int {
+			return old * 19
+		},
+		testMod:    23,
+		trueThrow:  2,
+		falseThrow: 3,
+	}, {
+		items: []int{54, 65, 75, 74},
+		operation: func(old int) int {
+			return old + 6
+		},
+		testMod:    19,
+		trueThrow:  2,
+		falseThrow: 0,
+	}, {
+		items: []int{79, 60, 97},
+		operation: func(old int) int {
+			return old * old
+		},
+		testMod:    13,
+		trueThrow:  1,
+		falseThrow: 3,
+	}, {
+		items: []int{74},
+		operation: func(old int) int {
+			return old + 3
+		},
+		testMod:    17,
+		trueThrow:  0,
+		falseThrow: 1,
+	},
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "testinput",
+			args: args{monkeys: testMonkeys},
+			want: 2713310158,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part02(tt.args.monkeys); got != tt.want {
+				t.Errorf("part02() = %v, want %v", got, tt.want)
 			}
 		})
 	}
